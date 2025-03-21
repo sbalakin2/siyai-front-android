53161dc32d0af3977458c285bca8f8ca2fc8d2ca
Evgenii Suvorov <suvorov.developer@gmail.com>
2025-03-21 11:21:11 +0300

    Add ValidationUserData

---
+++ app/src/main/java/com/example/siyai_front_android/utils/ValidationUserData.kt
@@ -0,0 +1,30 @@
+package com.example.siyai_front_android.utils
+
+import android.content.Context
+import com.example.siyai_front_android.R
+
+fun validateUserData(
+    email: String,
+    password: String,
+    repeatPassword: String? = null,
+    context: Context,
+    isRegistration: Boolean = false
+): String? {
+    return when {
+        email.isEmpty() -> {
+            context.getString(R.string.email_empty)
+        }
+        !email.isValidEmail() -> {
+            context.getString(R.string.invalid_email)
+        }
+        !password.isValidPassword() -> {
+            context.getString(R.string.invalid_password)
+        }
+        isRegistration && repeatPassword != password -> {
+            context.getString(R.string.invalid_repeat_password)
+        }
+        else -> {
+            null
+        }
+    }
+}
