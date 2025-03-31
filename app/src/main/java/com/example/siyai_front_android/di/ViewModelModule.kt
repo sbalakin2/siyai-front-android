49a7de6962d5e7497e2cea05d05dc41515b33607
Evgenii Suvorov <suvorov.developer@gmail.com>
2025-03-31 14:17:29 +0300

    Add ViewModelKey, add ViewModelModule

---
+++ app/src/main/java/com/example/siyai_front_android/di/ViewModelModule.kt
@@ -0,0 +1,20 @@
+package com.example.siyai_front_android.di
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.siyai_front_android.presentation.reg.RegViewModel
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+
+@Module(includes = [UseCaseModule::class])
+abstract class ViewModelModule {
+
+    @Binds
+    abstract fun bindViewModelFactory(viewModelFactory: ViewModelFactory): ViewModelProvider.Factory
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(RegViewModel::class)
+    abstract fun bindRegViewModel(regViewModel: RegViewModel): ViewModel
+}
