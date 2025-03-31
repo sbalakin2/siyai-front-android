d02e3ff3948c36c6747e92c580f5ebe387500a4e
Evgenii Suvorov <suvorov.developer@gmail.com>
2025-03-31 14:11:08 +0300

    Add UseCaseModule, add bindRegUseCase

---
+++ app/src/main/java/com/example/siyai_front_android/di/UseCaseModule.kt
@@ -0,0 +1,12 @@
+package com.example.siyai_front_android.di
+
+import com.example.siyai_front_android.domain.usecases.RegUseCase
+import com.example.siyai_front_android.domain.usecases.RegUseCaseImpl
+import dagger.Binds
+import dagger.Module
+
+@Module(includes = [DataModule::class])
+interface UseCaseModule {
+    @Binds
+    fun bindRegUseCase(regUseCase: RegUseCaseImpl): RegUseCase
+}
