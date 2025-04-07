a3fb3c15ce113341ce098370b008985ffc43beca
Nikolay Vereshchagin <nick.vereshchagin@gmail.com>
2025-04-07 12:36:24 +0300

    Changed DropDownTextField

--- app/src/main/java/com/example/siyai_front_android/ui/components/text_fields/DropDownTextField.kt
+++ app/src/main/java/com/example/siyai_front_android/ui/components/text_fields/DropDownTextField.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.DropdownMenu
@@ -14,15 +13,12 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -59,7 +55,7 @@ fun <T : Any> DropDownTextField(
         modifier = modifier
     ) {
 
-        TextField(
+        BaseTextField(
             value = itemLabel(value),
             onValueChange = {},
             modifier = Modifier
@@ -68,27 +64,18 @@ fun <T : Any> DropDownTextField(
                 .height(64.dp),
             enabled = enabled,
             readOnly = true,
-            label = { Text(label) },
+            label = label,
             placeholder = placeholder,
             leadingIcon = leadingIcon,
             trailingIcon = trailingIcon,
             prefix = prefix,
             suffix = suffix,
             supportingText = supportingText,
-            textStyle = MaterialTheme.typography.bodyLarge.copy(
-                color = MaterialTheme.colorScheme.onSurfaceVariant
-            ),
             visualTransformation = visualTransformation,
             keyboardOptions = keyboardOptions,
             keyboardActions = keyboardActions,
             interactionSource = interactionSource,
-            singleLine = true,
             isError = isError,
-            shape = RoundedCornerShape(14.dp),
-            colors = TextFieldDefaults.colors(
-                focusedIndicatorColor = Color.Transparent,
-                unfocusedIndicatorColor = Color.Transparent,
-            )
         )
 
         DropdownMenu(
