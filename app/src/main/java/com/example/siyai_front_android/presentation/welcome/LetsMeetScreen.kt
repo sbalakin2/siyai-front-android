83f0f5d55f3c53d9b3c0e6983eda3ead7969bff9
Nikolay Vereshchagin <nick.vereshchagin@gmail.com>
2025-04-07 12:37:59 +0300

    Add DatePicker and TopAppBar

--- app/src/main/java/com/example/siyai_front_android/presentation/welcome/LetsMeetScreen.kt
+++ app/src/main/java/com/example/siyai_front_android/presentation/welcome/LetsMeetScreen.kt
@@ -8,9 +8,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
@@ -25,26 +27,33 @@ import androidx.compose.ui.unit.dp
 import com.example.siyai_front_android.R
 import com.example.siyai_front_android.ui.components.buttons.PrimaryButton
 import com.example.siyai_front_android.ui.components.text_fields.BaseTextField
+import com.example.siyai_front_android.ui.components.text_fields.DatePickerTextField
 import com.example.siyai_front_android.ui.components.text_fields.DropDownTextField
 import com.example.siyai_front_android.ui.theme.SiyaifrontandroidTheme
+import java.util.Date
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LetsMeetScreen() {
 
     var userName by rememberSaveable { mutableStateOf("") }
     var userSurname by rememberSaveable { mutableStateOf("") }
     var city by rememberSaveable { mutableStateOf("") }
-    var userBirthday by rememberSaveable { mutableStateOf("") }
+    var userBirthday by rememberSaveable { mutableStateOf<Date?>(null) }
     var country by rememberSaveable { mutableStateOf("") }
 
     val isEnterToAppEnabled by remember {
         derivedStateOf {
-            sequenceOf(userName, userSurname, city, userBirthday, country)
-                .all { it.isNotEmpty() }
+            sequenceOf(userName, userSurname, city, country)
+                .all { it.isNotEmpty() } && userBirthday != null
         }
     }
 
-    Scaffold { paddingValues ->
+    Scaffold(
+        topBar = {
+            TopAppBar(title = {})
+        }
+    ) { paddingValues ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -86,8 +95,7 @@ fun LetsMeetScreen() {
                     .padding(vertical = 6.dp)
             )
 
-            // TODO(Date selector)
-            BaseTextField(
+            DatePickerTextField(
                 value = userBirthday,
                 onValueChange = { value -> userBirthday = value },
                 label = stringResource(R.string.lets_meet_birthday),
