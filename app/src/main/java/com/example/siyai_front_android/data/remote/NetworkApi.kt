beebd6b5f3e9e64413c3e571d41043150624716b
Evgenii <aiisagurov@gmail.com>
2025-04-09 09:14:35 +0300

    Add verify

--- app/src/main/java/com/example/siyai_front_android/data/remote/NetworkApi.kt
+++ app/src/main/java/com/example/siyai_front_android/data/remote/NetworkApi.kt
@@ -1,14 +1,16 @@
 package com.example.siyai_front_android.data.remote
 
 import com.example.siyai_front_android.data.remote.dto.LoginRequest
+import com.example.siyai_front_android.data.remote.dto.VerificationResponse
 import com.example.siyai_front_android.data.remote.dto.RegRequest
+import com.example.siyai_front_android.data.remote.dto.VerificationRequest
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface NetworkApi {
-    @POST("/user-service/v1/registration")
+    @POST("user-service/v1/registration")
     suspend fun registerUser(
         @Header("Authorization") authorization: String,
         @Body regRequest: RegRequest
@@ -19,4 +21,10 @@ interface NetworkApi {
         @Header("Authorization") authorization: String,
         @Body loginRequest: LoginRequest
     ): Response<Unit>
+
+    @POST("message-service/v1/otp")
+    suspend fun verify(
+        @Header("Authorization") authorization: String,
+        @Body verificationRequest: VerificationRequest
+    ): Response<VerificationResponse>
 }
