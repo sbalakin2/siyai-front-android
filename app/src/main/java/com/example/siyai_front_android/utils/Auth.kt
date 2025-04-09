b0659ebe671eaa76585b88fc2980de454f047f3b
Evgenii <aiisagurov@gmail.com>
2025-04-09 09:07:55 +0300

    Add values in parameters email and password

--- app/src/main/java/com/example/siyai_front_android/utils/Auth.kt
+++ app/src/main/java/com/example/siyai_front_android/utils/Auth.kt
@@ -2,7 +2,7 @@ package com.example.siyai_front_android.utils
 
 import android.util.Base64
 
-fun createCredentials(email: String, password: String): String {
+fun createCredentials(email: String = "shine_admin", password: String = "p7UPX0}~;–LJr?_"): String {
     val credentials = "$email:$password"
     val encodedCredentials = Base64.encodeToString(credentials.toByteArray(), Base64.NO_WRAP)
     return "Basic $encodedCredentials"
