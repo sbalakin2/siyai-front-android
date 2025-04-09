13b7aea228d52bc663c995aff48bdc688031112b
Evgenii <aiisagurov@gmail.com>
2025-04-09 09:22:15 +0300

    Replace regUseCase with verifyUseCase

--- app/src/main/java/com/example/siyai_front_android/presentation/reg/RegViewModel.kt
+++ app/src/main/java/com/example/siyai_front_android/presentation/reg/RegViewModel.kt
@@ -2,7 +2,8 @@ package com.example.siyai_front_android.presentation.reg
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.siyai_front_android.domain.usecases.RegUseCase
+import com.example.siyai_front_android.domain.usecases.VerifyUseCase
+import com.example.siyai_front_android.presentation.email_confirmation.VerificationState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -10,15 +11,15 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class RegViewModel @Inject constructor(
-    private val regUseCase: RegUseCase
+    private val verifyUseCase: VerifyUseCase
 ): ViewModel() {
 
-    private val _regState = MutableStateFlow<RegState>(RegState.Loading)
-    val regState: StateFlow<RegState> = _regState.asStateFlow()
+    private val _verificationState = MutableStateFlow<VerificationState>(VerificationState.Idle)
+    val verificationState: StateFlow<VerificationState> = _verificationState.asStateFlow()
 
-    fun registerUser(email: String, password: String) {
+    fun verify(email: String) {
         viewModelScope.launch {
-            _regState.value = regUseCase(email, password)
+            _verificationState.value = verifyUseCase(email)
         }
     }
 }
