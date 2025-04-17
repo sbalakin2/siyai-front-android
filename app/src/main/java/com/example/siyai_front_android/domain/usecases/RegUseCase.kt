b4a78d8169a96b9a512ae943b046c5e145cd557a
murazin-dn <megashlanggggg@gmail.com>
2025-04-18 02:37:12 +0500

    fix merge conflict

--- app/src/main/java/com/example/siyai_front_android/domain/usecases/RegUseCase.kt
+++ app/src/main/java/com/example/siyai_front_android/domain/usecases/RegUseCase.kt
@@ -1,6 +1,6 @@
 package com.example.siyai_front_android.domain.usecases
 
-import com.example.siyai_front_android.presentation.auth.reg.RegState
+import com.example.siyai_front_android.presentation.auth.email_confirmation.RegState
 
 interface RegUseCase {
     suspend operator fun invoke(email: String, password: String): RegState
