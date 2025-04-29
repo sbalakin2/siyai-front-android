029e6cd4d321f6207222df1f62a46d24d6ab305d
Evgenii <aiisagurov@gmail.com>
2025-04-29 17:07:11 +0300

    Add checkIsFormCompleted

--- app/src/main/java/com/example/siyai_front_android/utils/ValidationUserData.kt
+++ app/src/main/java/com/example/siyai_front_android/utils/ValidationUserData.kt
@@ -1,7 +1,10 @@
 package com.example.siyai_front_android.utils
 
 import android.content.Context
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
 import com.example.siyai_front_android.R
+import java.util.Date
 
 fun validateUserData(
     email: String? = null,
@@ -14,4 +17,15 @@ fun validateUserData(
     repeatPassword?.let {
         if (repeatPassword != password) return context.getString(R.string.invalid_repeat_password)}
     return null
+}
+
+fun checkIsFormCompleted(
+    firstName: String,
+    lastName: String,
+    birthday: Date?,
+    city: String,
+    country: String
+): Boolean {
+    return sequenceOf(firstName, lastName, city, country)
+        .all { it.isNotEmpty() } && birthday != null
 }
