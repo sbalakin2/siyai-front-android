af65c8a57033bad02da86531d982ec24cf20abcc
Evgenii <aiisagurov@gmail.com>
2025-05-06 18:29:09 +0300

    Add Profile

---
+++ app/src/main/java/com/example/siyai_front_android/domain/dto/Profile.kt
@@ -0,0 +1,10 @@
+package com.example.siyai_front_android.domain.dto
+
+data class Profile(
+    val firstName: String,
+    val lastName: String,
+    val birthday: String,
+    val email: String,
+    val country: String,
+    val city: String
+)
