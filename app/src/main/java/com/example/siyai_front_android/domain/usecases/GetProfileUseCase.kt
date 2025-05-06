55cf6bc4816e612c048adda85cfb968b23032202
Evgenii <aiisagurov@gmail.com>
2025-05-06 18:29:42 +0300

    Add GetProfileUseCase

---
+++ app/src/main/java/com/example/siyai_front_android/domain/usecases/GetProfileUseCase.kt
@@ -0,0 +1,7 @@
+package com.example.siyai_front_android.domain.usecases
+
+import com.example.siyai_front_android.presentation.profile.ProfileState
+
+interface GetProfileUseCase {
+    suspend operator fun invoke(email: String): ProfileState
+}
