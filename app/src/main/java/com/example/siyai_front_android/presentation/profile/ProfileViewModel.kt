2434e42938a69a6fe1c3110c74712588455e893c
Evgenii <aiisagurov@gmail.com>
2025-05-07 15:23:36 +0300

    Remove init

--- app/src/main/java/com/example/siyai_front_android/presentation/profile/ProfileViewModel.kt
+++ app/src/main/java/com/example/siyai_front_android/presentation/profile/ProfileViewModel.kt
@@ -3,7 +3,6 @@ package com.example.siyai_front_android.presentation.profile
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.siyai_front_android.domain.usecases.GetProfileUseCase
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -16,10 +15,6 @@ class ProfileViewModel @Inject constructor(
 
     private val _profileState = MutableStateFlow<ProfileState>(ProfileState.Loading)
     val profileState: StateFlow<ProfileState> = _profileState.asStateFlow()
-
-    init {
-        getProfile("test@mail.com")
-    }
 
     fun getProfile(email: String) {
         viewModelScope.launch {
