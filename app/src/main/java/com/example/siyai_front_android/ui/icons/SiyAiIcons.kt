052632f8f7c23eb4baa0e21d77e98bd24be3ad29
Evgenii <aiisagurov@gmail.com>
2025-07-04 12:14:44 +0300

    Add Pause, Replay10, Forward30

--- app/src/main/java/com/example/siyai_front_android/ui/icons/SiyAiIcons.kt
+++ app/src/main/java/com/example/siyai_front_android/ui/icons/SiyAiIcons.kt
@@ -1,7 +1,10 @@
 package com.example.siyai_front_android.ui.icons
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Forward30
+import androidx.compose.material.icons.filled.Pause
 import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Replay10
 
 object SiyAiIcons {
     val ArrowBack = ArrowBackIcon
@@ -27,5 +30,8 @@ object SiyAiIcons {
     val Forward = ForwardIcon
     val Trash = TrashIcon
     val PlayArrow = Icons.Default.PlayArrow
+    val Pause = Icons.Default.Pause
+    val Replay10 = Icons.Default.Replay10
+    val Forward30 = Icons.Default.Forward30
     val Stop = StopIcon
 }
